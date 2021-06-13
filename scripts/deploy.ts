5e99e0503c453bb6d0665ce208ed1a3b41b40d80
chiranz <chiranpoudyal@gmail.com>
2021-06-13 07:44:30 +0530

    Added helper functions to automatically save address and network of contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,23 @@
 import "@nomiclabs/hardhat-ethers";
 import hardhat, { ethers } from "hardhat";
+import { networkName } from "../helpers/hardhat_helper";
+import { saveDeployedAddress } from "./utils";
 
 async function main() {
   await hardhat.run("compile");
 
-  // We get the contract to deploy
+  // DEPLOY CHI TOKEN
   const ChiToken = await ethers.getContractFactory("ERC20");
   const chiToken = await ChiToken.deploy("CHI Token", "CHI", 1000000);
 
   await chiToken.deployed();
-  console.log("CHI token deployed to : " + chiToken.address);
-  const DaiToken = await ethers.getContractFactory("ERC20");
-  const daiToken = await ChiToken.deploy("Dai Token", "DAI", 1000000);
+  console.log("CHI token deployed to address : " + chiToken.address);
+  let { chainId } = chiToken.deployTransaction;
+  console.log("Network: " + networkName[chainId]);
+  let contractName = "chiToken";
 
-  await chiToken.deployed();
-  console.log("CHI token deployed to : " + chiToken.address);
+  saveDeployedAddress({ contractName, contract: chiToken });
+  console.log(`${contractName} deployed details saved successfully.`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
